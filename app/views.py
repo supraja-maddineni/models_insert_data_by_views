72216e653b843e8799ad2bfe4a4b7dba1b97a222
supraja-maddineni <suprajamaddineni345@gmail.com>
2023-04-03 17:42:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app.models  import *
+
+# Create your views here.
+def insert_Topic(request):
+    tn=input('enter topic_name: ')
+    TO=Topic.objects.get_or_create(topic_name=tn)[0]
+    TO.save()
+    return HttpResponse('Topic Inserted Successfully')
+
+def insert_Webpage(request):
+    tn=input('enter Topic_name: ')
+    name=input('enter name: ')
+    url=input('enter url: ')
+    TO=Topic.objects.get_or_create(topic_name=tn)[0]
+    TO.save()
+    WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url)[0]
+    WO.save()
+    return HttpResponse('Webpage Data Inserted Successfully')
+
+def insert_AccessRecord(request):
+    tn=input('enter Topic_name: ')
+    name=input('enter name: ')
+    url=input('enter url: ')
+    author=input('enter author name: ')
+    date=input('enter date: ')
+    TO=Topic.objects.get_or_create(topic_name=tn)[0]
+    TO.save()
+    WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url)[0]
+    WO.save()
+    AO=AccessRecord.objects.get_or_create(name=WO,author=author,date=date)[0]
+    AO.save()
+    return HttpResponse('AccessRecord  Inserted Successfully')
+
